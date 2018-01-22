98b7cb5f04164dea600fb26ce4149bb45c113527
Alice Tang <Alice@My-Computer.local>
2018-01-21 20:00:32 -0500

    first push

--- First_Push.php
+++ First_Push.php
@@ -15,6 +15,7 @@
 			  $("#Michael").val("Michael Tang");
                 $("#Karl").val("Karl Joey Chami");
                  $("#Berfin").val("Berfin Saricam");
+                    $("#Wu").val("Wu Wen Tang");
             
             $("#button").click(function(){
                 document.write("If you can see this text you can Commit and Push");
@@ -28,7 +29,8 @@
 <p>Name: <input id="Yanis" type="text"></p>
 <p>Name: <input id="Michael" type="text"></p>
 <p>Name: <input id="Karl" type="text"></p>    
-<p>Name: <input id="Berfin" type="text"></p>    
+<p>Name: <input id="Berfin" type="text"></p>  
+<p>Name: <input id="Wu" type="text"></p> 
 
 <button id="button" class="button">Verify</button>
 </body>
