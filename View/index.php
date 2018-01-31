f506f2b35eabf4e9806eb9f19bf1cb3e45bdb175
skanderbm123 <skander96@hotmail.com>
2018-01-30 21:53:13 -0500

    [add text to index.php][#39]I added text to test if the main page display proper text

--- View/index.php
+++ View/index.php
@@ -1,7 +1,11 @@
+<!DOCTYPE html>
 <html>
 <head>
 <title>Main Page</title>
+
 </head>
 <body>
+
+<p> We are going to work on the main page . We are going to add a login and register button for the next sprint. This is only a test line for the MAIN.</p>
 </body>
 </html>
