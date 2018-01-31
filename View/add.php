04b6d91d8f2841fbd31d453ffbeb03d4655d360a
wuwentang <wuwen_tang@hotmail.com>
2018-01-30 22:09:42 -0500

    [added text in add.php][#41] added text

--- View/add.php
+++ View/add.php
@@ -2,11 +2,12 @@
 
 	<html>
 		<head>
+			<title> Add </title>
 
-</head>
+		</head>
 
-			<body>
-
+		<body>
+			<p>adding lines in ADD page, will be replace with front-end layout for sprint 2 </p>
  
-			</body>
+		</body>
 	</html>
