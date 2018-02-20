6fe4c9a66b093d97328c8e65138a376a2135faa1
Yanis <Sibachir>
2018-02-20 15:39:34 -0500

    [debug feathure][#111]debugg the upvote reply, user can now upvote and downvote once

--- Controller/addreply.php
+++ Controller/addreply.php
@@ -2,11 +2,18 @@
 session_start();
 include ("../config/db_server.php");
 
-$desc= $_POST['desc'];
 
-$db = new DB();
+$return = array();
+$return[0]=false;
+if(isset($_SESSION['username']))
+    if($_SESSION["username"]!=null) {
+        $desc = $_POST['desc'];
 
-$db->query("insert into replylist (description_reply,total_positive,total_negative,IDQuestion,validate,username) values('".$desc."','0','0','".$_SESSION['idquestion']."','0','".$_SESSION['username']."') ");
-$db->close();
+        $db = new DB();
 
+        $db->query("insert into replylist (description_reply,total_positive,total_negative,IDQuestion,validate,username) values('" . $desc . "','0','0','" . $_SESSION['idquestion'] . "','0','" . $_SESSION['username'] . "') ");
+        $db->close();
+        $return[0]=true;
+    }
+    echo json_encode($return);
 ?>
--- Controller/upvoteReply.php
+++ Controller/upvoteReply.php
@@ -6,14 +6,36 @@
  * Time: 8:54 PM
  */
 include ("../config/db_server.php");
+session_start();
+if(isset($_SESSION['username']))
+    if($_SESSION["username"]!=null) {
 
-$valueIDTOadd = $_POST['value'];
-$db= new DB();
 
-$result= $db->query("select * from replylist where ID ='$valueIDTOadd'");
+        $valueIDTOadd = $_POST['value'];
+        $db = new DB();
+        $user = $_SESSION['username'];
+        $ifuserexist = $db->query("select * from user where username='$user'");
+        $row = $ifuserexist->fetch_assoc();
+        $valueOfReplyList = $row['reply_list'];
+        $valueOfReplyListinarray = explode(",",$row['reply_list']);
 
-$row = $result->fetch_assoc();
-$valueofTotalpositif = $row['total_positive'] +1;
-$db->query("UPDATE replylist SET total_positive='$valueofTotalpositif' WHERE ID='$valueIDTOadd'");
+        if(!in_array($valueIDTOadd,$valueOfReplyListinarray)) {
 
-$db->close();
+            $result = $db->query("select * from replylist where ID ='$valueIDTOadd'");
+
+            $row = $result->fetch_assoc();
+            $valueofTotalpositif = $row['total_positive'] + 1;
+            $db->query("UPDATE replylist SET total_positive='$valueofTotalpositif' WHERE ID='$valueIDTOadd'");
+
+
+
+
+            if($valueOfReplyList!="")
+            $valueOfReplyList.=",".$valueIDTOadd;
+            else $valueOfReplyList = $valueIDTOadd;
+
+            $db->query("update user set reply_list = '$valueOfReplyList' where username ='".$_SESSION['username']."'");
+
+        }
+        $db->close();
+    }
