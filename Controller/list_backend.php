9ea55a3aa7d68023ad012f70ef0d993cc5dfc527
kasthurie <sriahila@hotmail.com>
2018-03-02 21:29:50 -0500

    [Feature related question][#92] added related question when question is clicked on based on tags

--- Controller/list_backend.php
+++ Controller/list_backend.php
@@ -1,6 +1,22 @@
 <?php
 include("../config/db_server.php");
 
+
+function mail_utf8($to, $from_user, $from_email, 
+                                             $subject = '(No subject)', $message = '')
+   { 
+      $from_user = "=?UTF-8?B?".base64_encode($from_user)."?=";
+      $subject = "=?UTF-8?B?".base64_encode($subject)."?=";
+
+      $headers = "From: $from_user <$from_email>\r\n". 
+               "MIME-Version: 1.0" . "\r\n" . 
+               "Content-type: text/html; charset=UTF-8" . "\r\n"; 
+
+     return mail($to, $subject, $message, $headers); 
+   }
+   mail_utf8("phistan007@gmail.com", "domain@domain.com", "domain@domain", "dada","dasd");
+
+
 $SearchUSer = $_POST['search'];
 $arrUser = explode(" ",$SearchUSer);
 
