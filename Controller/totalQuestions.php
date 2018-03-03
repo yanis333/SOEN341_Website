e2d66197d337c629b0a96c80a34a5a090cf33e8f
TangMichael <tangmichael6@hotmail.com>
2018-03-03 17:05:50 -0500

    [Bug related to total_quetions][#119] fixed the query to properly update the number

--- Controller/totalQuestions.php
+++ Controller/totalQuestions.php
@@ -3,7 +3,7 @@
 include ("../config/db_server.php");
 
 $db = new DB();
-
+        $db->query("UPDATE total_questions SET total = (SELECT COUNT(*)from questionlog)");
         $result = $db->query("select * from total_questions");
         $row = $result->fetch_assoc();
         echo json_encode($row);
