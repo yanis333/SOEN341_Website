114501e22e09d106a6728a03ce74ba4b7ca298b1
Berfin <berfin.saricam@gmail.com>
2018-03-03 20:37:59 -0500

    [Search Page Bug][#110] Search results are case insensitive. There are no longer duplicates that appear when searching for multiple keywords.

--- Controller/list_backend.php
+++ Controller/list_backend.php
@@ -2,64 +2,50 @@
 include("../config/db_server.php");
 
 
-function mail_utf8($to, $from_user, $from_email, 
-                                             $subject = '(No subject)', $message = '')
-   { 
-      $from_user = "=?UTF-8?B?".base64_encode($from_user)."?=";
-      $subject = "=?UTF-8?B?".base64_encode($subject)."?=";
-
-      $headers = "From: $from_user <$from_email>\r\n". 
-               "MIME-Version: 1.0" . "\r\n" . 
-               "Content-type: text/html; charset=UTF-8" . "\r\n"; 
-
-     return mail($to, $subject, $message, $headers); 
-   }
-   mail_utf8("phistan007@gmail.com", "domain@domain.com", "domain@domain", "dada","dasd");
-
-
 $SearchUSer = $_POST['search'];
 $arrUser = explode(" ",$SearchUSer);
-
+$arrUser = array_map('strtolower', $arrUser);
 
 $list =array("a", "as", "able", "about", "above", "according", "accordingly", "across", "actually", "after", "afterwards", "again", "against", "aint", "all", "allow", "allows", "almost", "alone", "along", "already", "also", "although", "always", "am", "among", "amongst", "an", "and", "another", "any", "anybody", "anyhow", "anyone", "anything", "anyway", "anyways", "anywhere", "apart", "appear", "appreciate", "appropriate", "are", "arent", "around", "as", "aside", "ask", "asking", "associated", "at", "available", "away", "awfully", "be", "became", "because", "become", "becomes", "becoming", "been", "before", "beforehand", "behind", "being", "believe", "below", "beside", "besides", "best", "better", "between", "beyond", "both", "brief", "but", "by", "cmon", "cs", "came", "can", "cant", "cannot", "cant", "cause", "causes", "certain", "certainly", "changes", "clearly", "co", "com", "come", "comes", "concerning", "consequently", "consider", "considering", "contain", "containing", "contains", "corresponding", "could", "couldnt", "course", "currently", "definitely", "described", "despite", "did", "didnt", "different", "do", "does", "doesnt", "doing", "dont", "done", "down", "downwards", "during", "each", "edu", "eg", "eight", "either", "else", "elsewhere", "enough", "entirely", "especially", "et", "etc", "even", "ever", "every", "everybody", "everyone", "everything", "everywhere", "ex", "exactly", "example", "except", "far", "few", "ff", "fifth", "first", "five", "followed", "following", "follows", "for", "former", "formerly", "forth", "four", "from", "further", "furthermore", "get", "gets", "getting", "given", "gives", "go", "goes", "going", "gone", "got", "gotten", "greetings", "had", "hadnt", "happens", "hardly", "has", "hasnt", "have", "havent", "having", "he", "hes", "hello", "help", "hence", "her", "here", "heres", "hereafter", "hereby", "herein", "hereupon", "hers", "herself", "hi", "him", "himself", "his", "hither", "hopefully", "how", "howbeit", "however", "i", "id", "ill", "im", "ive", "ie", "if", "ignored", "immediate", "in", "inasmuch", "inc", "indeed", "indicate", "indicated", "indicates", "inner", "insofar", "instead", "into", "inward", "is", "isnt", "it", "itd", "itll", "its", "its", "itself", "just", "keep", "keeps", "kept", "know", "knows", "known", "last", "lately", "later", "latter", "latterly", "least", "less", "lest", "let", "lets", "like", "liked", "likely", "little", "look", "looking", "looks", "ltd", "mainly", "many", "may", "maybe", "me", "mean", "meanwhile", "merely", "might", "more", "moreover", "most", "mostly", "much", "must", "my", "myself", "name", "namely", "nd", "near", "nearly", "necessary", "need", "needs", "neither", "never", "nevertheless", "new", "next", "nine", "no", "nobody", "non", "none", "noone", "nor", "normally", "not", "nothing", "novel", "now", "nowhere", "obviously", "of", "off", "often", "oh", "ok", "okay", "old", "on", "once", "one", "ones", "only", "onto", "or", "other", "others", "otherwise", "ought", "our", "ours", "ourselves", "out", "outside", "over", "overall", "own", "particular", "particularly", "per", "perhaps", "placed", "please", "plus", "possible", "presumably", "probably", "provides", "que", "quite", "qv", "rather", "rd", "re", "really", "reasonably", "regarding", "regardless", "regards", "relatively", "respectively", "right", "said", "same", "saw", "say", "saying", "says", "second", "secondly", "see", "seeing", "seem", "seemed", "seeming", "seems", "seen", "self", "selves", "sensible", "sent", "serious", "seriously", "seven", "several", "shall", "she", "should", "shouldnt", "since", "six", "so", "some", "somebody", "somehow", "someone", "something", "sometime", "sometimes", "somewhat", "somewhere", "soon", "sorry", "specified", "specify", "specifying", "still", "sub", "such", "sup", "sure", "ts", "take", "taken", "tell", "tends", "th", "than", "thank", "thanks", "thanx", "that", "thats", "thats", "the", "their", "theirs", "them", "themselves", "then", "thence", "there", "theres", "thereafter", "thereby", "therefore", "therein", "theres", "thereupon", "these", "they", "theyd", "theyll", "theyre", "theyve", "think", "third", "this", "thorough", "thoroughly", "those", "though", "three", "through", "throughout", "thru", "thus", "to", "together", "too", "took", "toward", "towards", "tried", "tries", "truly", "try", "trying", "twice", "two", "un", "under", "unfortunately", "unless", "unlikely", "until", "unto", "up", "upon", "us", "use", "used", "useful", "uses", "using", "usually", "value", "various", "very", "via", "viz", "vs", "want", "wants", "was", "wasnt", "way", "we", "wed", "well", "were", "weve", "welcome", "well", "went", "were", "werent", "what", "whats", "whatever", "when", "whence", "whenever", "where", "wheres", "whereafter", "whereas", "whereby", "wherein", "whereupon", "wherever", "whether", "which", "while", "whither", "who", "whos", "whoever", "whole", "whom", "whose", "why", "will", "willing", "wish", "with", "within", "without", "wont", "wonder", "would", "would", "wouldnt", "yes", "yet", "you", "youd", "youll", "youre", "youve", "your", "yours", "yourself", "yourselves", "zero");
 
+$list = array_map('strtolower', $list);
+
 $goodarrUser = array();
+
 for($check=0;$check<count($arrUser);$check++){
     
     if(in_array($arrUser[$check],  $list)){
         
     }
     else{
-        $goodarrUser[] = $arrUser[$check];
+        $goodarrUser[] = $arrUser[$check]; // has the searched terms  youre looking for in the database
     }
 }
 
 $db = new DB();
 $finalList = array();
+$finalList = array_map('strtolower', $finalList);
 $result = $db->query("select * from questionlog");
 while($row = $result->fetch_assoc()){
     $goodarrDb = array();
   
-    $arrayData= explode(" ",$row['title']);
+    $arrayData= explode(" ",$row['title']); // the words in the title of the question database that you go through
 
-  for($check=0;$check<count($arrayData);$check++){
+  for($check=0;$check<count($arrayData);$check++){ // for loop the words in the title of the database
     
-    if(in_array($arrayData[$check],$list)){
+    if(in_array($arrayData[$check],$list)){ // if the word is inside the list of words we dont want, do nothing 
         
     }
     else{
-        $goodarrDb[] = $arrayData[$check];
+        $goodarrDb[] = $arrayData[$check]; // put the one of the word of the title in the goodarrDb // you know the word is a word that needs to be checked
+    if(in_array($arrayData[$check],$goodarrUser)){ // check if the word is in the array of words the user searched for
+        $finalList[] = $row; // if it is put it in the array you give back and break because you dont need to check the other words. --> proceed to the next row in the db
+        break;
+    }
     }
         
     }
 
-
-
-  for($check=0;$check<count($goodarrUser);$check++){
-    if(in_array($goodarrUser[$check],$goodarrDb)){
-        $finalList[] = $row;
-    }
-    }
     
 }
 echo json_encode($finalList);
